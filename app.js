0054f017e32df7ffe56383d336f6b7c62454f552
ChinjiroO <techinseoikrathok@gmail.com>
2022-01-07 15:03:57 +0700

    add draw()

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
-var myConst = {
+const myConst = {
   cellElement: document.querySelectorAll(".cell"),
   playerTurn: document.getElementById("playerTurn"),
+  winning_combinations: [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8],
+  ],
 };
+
+const arr = [...myConst.cellElement];
 let turn = true;
 start();
 
 function start() {
-  myConst.cellElement.forEach(function (cell) {
+  arr.forEach(function (cell, index) {
+    console.log(`${index}: ${cell.classList.length}`);
     myConst.playerTurn.innerHTML = "Player 1 turn";
     cell.classList.remove("cross");
     cell.classList.remove("circle");
@@ -15,7 +28,6 @@ function start() {
     // TODO: check classList of element if !== circle then add circle class
   });
 }
-
 function handleClick(e) {
   const cell = e.target;
   const mask = turn ? "cross" : "circle";
@@ -26,6 +38,22 @@ function handleClick(e) {
     myConst.playerTurn.innerHTML = "Player 2 turn";
   }
   turn = !turn;
+
+  // * comment line below------
+  window.console.clear();
+  arr.forEach(function (cell, index) {
+    console.log(`${index}: ${cell.classList.length}`);
+  });
+  // * comment line above------
+
+  draw();
+  if (draw()) {
+    window.alert("draw!!!");
+  }
+}
+function draw(a) {
+  // every cell that has length of classList > 2
+  return arr.every((cell) => cell.classList.length > 2);
 }
 
 function addMask(cell, mask) {
